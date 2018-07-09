139c6d18668ffe34427364f06bb817152e786dfa
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-09 22:46:14 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,4 +56,9 @@ class Student
     self.new_from_db(result)
   end
 
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
 end
