9c41af149c139afe56ca9db3e364d67fbba2872f
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-09 22:02:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,8 +11,15 @@ class Student
     @id = id
   end
 
-  def create_table
-
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+      SQL
+    DB[:conn].execute(sql)
   end
 
   def drop_table
